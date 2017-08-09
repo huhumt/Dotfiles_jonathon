a917fe313103747bc532297de3d4d32601660e56
Jonathan Hodgson <jonathan@lunarweb.co.uk>
2017-08-09 13:15:49 +0100

    Add dotjs and dotcss

---
+++ chromeExtentions/dotjs/dotjs.js
@@ -0,0 +1,27 @@
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://dotjs.local/' + window.location.host.replace(/^www\./, '') + '.js');
+
+xhr.onreadystatechange = function() {
+	if(xhr.readyState == 4) {
+		if(xhr.status == 200) {
+			var script = document.createElement('script');
+			script.textContent = xhr.responseText;
+			document.head.appendChild(script);
+			return;
+		} else if(xhr.status == 404) {
+			var defaultXHR = new XMLHttpRequest();
+			defaultXHR.open('GET', 'https://dotjs.local/default.js');
+			defaultXHR.onreadystatechange = function() {
+				if (defaultXHR.readyState == 4 && defaultXHR.status == 200){
+					var script = document.createElement('script');
+					script.textContent = defaultXHR.responseText;
+					document.head.appendChild(script);
+					return;
+				}
+			}
+			defaultXHR.send();
+		}
+	}
+};
+
+xhr.send();
