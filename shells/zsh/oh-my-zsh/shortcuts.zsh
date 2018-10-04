3c0723433e00abdada06402b578fa06c81a9004f
Jonathan Hodgson <jonathan@lunarweb.co.uk>
2018-10-04 16:51:25 +0100

    Update zsh config

---
+++ shells/zsh/oh-my-zsh/shortcuts.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+function append_date() { 
+  # Prepend "info" to the command line and run it.
+  BUFFER="$BUFFER-$(date '+%Y-%m-%d')"
+  zle end-of-line
+}
+
+# Define a widget called "run_info", mapped to our function above.
+zle -N append_date
+
+# Bind it to ESC-i.
+bindkey "" append_date
