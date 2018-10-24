78ebee2cf263bc3d77456107a01f0b64cee579ed
Jonathan Hodgson <jonathan@lunarweb.co.uk>
2018-10-24 17:37:47 +0100

    Add style and script for bitbucket

--- config/dotjs/bitbucket.org.js
+++ config/dotjs/bitbucket.org.js
@@ -2,7 +2,6 @@
 	const domLoaded = function domLoaded(){
 		document.removeEventListener("readystatechange",domLoaded);
 		document.querySelectorAll("li").forEach(li => {
-			console.log(li);
 			if( li.innerHTML.indexOf( '[' ) !== -1 ){
 				li.innerHTML = li.innerHTML.replace(/\[ \]/g, '☐');
 				li.innerHTML = li.innerHTML.replace(/\[x\]/g, '☒');
@@ -10,7 +9,6 @@
 		} );
 	}
 
-	console.log("I get here");
 	if(document.readyState == "loading"){
 
 		//If the dom hasn't loaded, wait until it is
