67b7d629773bad94745a1944b07df143ac0da1dd
Jonathan Hodgson <git@jonathanh.co.uk>
2019-09-26 08:57:27 +0100

    Changes to zsh - ranger shortcut and alias for armory tool

---
+++ shells/zsh/oh-my-zsh-custom/rangercd.zsh
@@ -0,0 +1,12 @@
+# Use ranger to switch directories and bind it to ctrl-o
+rangercd () {
+    tmp="$(mktemp)"
+    ranger --choosedir="$tmp" "$@"
+    if [ -f "$tmp" ]; then
+        dir="$(cat "$tmp")"
+        rm -f "$tmp"
+        [ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"
+    fi
+}
+alias ranger="rangercd"
+bindkey -s '^o' 'ranger\n'
