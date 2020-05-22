08892c7fc28bd01ef4119614fdad9c3feae25df3
Jonathan Hodgson <git@jonathanh.co.uk>
2020-05-22 17:32:19 +0100

    Makes completion look better with descriptions etc

--- shells/zsh/includes/completion.zsh
+++ shells/zsh/includes/completion.zsh
@@ -3,8 +3,22 @@ fpath=("${ZSH_FOLDER}completion" $fpath)
 # Make completion work
 autoload -U compinit
 zstyle ':completion:*' menu select
-# Auto complete with case insenstivity
+## Auto complete with case insenstivity
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+# Completion style
+zstyle ':completion:*:matches' group 'yes'
+zstyle ':completion:*:options' description 'yes'
+zstyle ':completion:*:options' auto-description '%d'
+zstyle ':completion:*:corrections' format ' %F{green}-- %d (errors: %e) --%f'
+zstyle ':completion:*:descriptions' format ' %F{yellow}-- %d --%f'
+zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+zstyle ':completion:*:default' list-prompt '%S%M matches%s'
+zstyle ':completion:*' format ' %F{yellow}-- %d --%f'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' verbose yes
+
 zmodload zsh/complist
 compinit
 #
