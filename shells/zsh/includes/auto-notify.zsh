428a59cafd1d2e60c68564b26f1801cde22930a7
Jonathan Hodgson <git@jonathanh.co.uk>
2021-03-21 12:56:40 +0000

    Adds cat to auto-notify ignore list

--- shells/zsh/includes/auto-notify.zsh
+++ shells/zsh/includes/auto-notify.zsh
@@ -2,4 +2,4 @@ source "$ZSH_FOLDER/plugins/zsh-auto-notify/auto-notify.plugin.zsh"
 export AUTO_NOTIFY_THRESHOLD=5
 export AUTO_NOTIFY_TITLE="Hey! %command has just finished"
 export AUTO_NOTIFY_BODY="It completed in %elapsed seconds with exit code %exit_code"
-AUTO_NOTIFY_IGNORE+=("bat" "zathura" "libreoffice" "lf" "davmail" "neomutt" "newsboat" "w3m" "sl")
+AUTO_NOTIFY_IGNORE+=("bat" "cat" "zathura" "libreoffice" "lf" "davmail" "neomutt" "newsboat" "w3m" "sl")
