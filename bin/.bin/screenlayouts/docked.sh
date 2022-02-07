1a419c86702e4f65d2802801823cb13a56d66edf
Jonathan Hodgson <git@jonathanh.co.uk>
2022-02-07 12:35:50 +0000

    Auto detect laptop resolution for screen layouts
    New laptop is slightly taller than my last so updates the screenlayout
    scripts to detect the resolution automatically

--- bin/.bin/screenlayouts/docked.sh
+++ bin/.bin/screenlayouts/docked.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 laptopScreen="eDP-1"
+laptopResolution="$(xrandr | grep -A 1 "$laptopScreen" | sed -n 2p | awk '{print $1}')"
 externalScreen="$(xrandr | grep -Eo '^.* connected' | grep -v "$laptopScreen" |
 	cut -d ' ' -f 1)"
 externalResolution="$(xrandr | grep -A 1 "$externalScreen" | sed -n 2p | awk '{print $1}')"
 
 pkill compton
-xrandr --output "$laptopScreen" --primary --mode 1920x1080 --pos "${externalResolution%x*}x0" --rotate normal --output "$externalScreen" --mode "$externalResolution" --pos 0x0 --rotate normal
+xrandr --output "$laptopScreen" --primary --mode "$laptopResolution" --pos "${externalResolution%x*}x0" --rotate normal --output "$externalScreen" --mode "$externalResolution" --pos 0x0 --rotate normal
 /usr/bin/compton --config "$HOME/.config/picom/picom.conf" & disown
--- bin/.bin/screenlayouts/laptop-only.sh
+++ bin/.bin/screenlayouts/laptop-only.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 pkill compton
+laptopScreen="eDP-1"
+laptopResolution="$(xrandr | grep -A 1 "$laptopScreen" | sed -n 2p | awk '{print $1}')"
 xrandr | sed '/^ /d' | grep -Ev '(:|eDP-1)' | awk -v ORS=" " '{print "--output " $1 " --off"}' |
-	xargs xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+	xargs xrandr --output "$laptopScreen" --primary --mode "$laptopResolution" --pos 0x0 --rotate normal
 /usr/bin/compton --config "$HOME/.config/picom/picom.conf" & disown
