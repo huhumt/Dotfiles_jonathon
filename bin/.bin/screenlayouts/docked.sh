d01e1b57ae863acceb2a0f0746a1c9b6a96890db
Jonathan Hodgson <git@jonathanh.co.uk>
2022-10-13 16:59:33 +0100

    Bin: small tweaks to screen layout scripts

--- bin/.bin/screenlayouts/docked.sh
+++ bin/.bin/screenlayouts/docked.sh
@@ -4,7 +4,7 @@ laptopScreen="eDP-1"
 laptopResolution="$(xrandr | grep -A 1 "$laptopScreen" | sed -n 2p | awk '{print $1}')"
 externalScreen="$(xrandr | grep -Eo '^.* connected' | grep -v "$laptopScreen" |
 	cut -d ' ' -f 1)"
-externalResolution="$(xrandr | grep -A 1 "$externalScreen" | sed -n 2p | awk '{print $1}')"
+externalResolution="$(xrandr | grep -A 1 "^$externalScreen" | sed -n 2p | awk '{print $1}')"
 
 pkill compton
 xrandr --output "$laptopScreen" --primary --mode "$laptopResolution" --pos "${externalResolution%x*}x0" --rotate normal --output "$externalScreen" --mode "$externalResolution" --pos 0x0 --rotate normal
